d100c52dca24f90cb5f2662a3c98984f15f10793
forevel <vao@asu-vei.ru>
2015-07-19 21:02:30 +0300

    #0255_1

--- dialogs/sysdlg/systableeditor.cpp
+++ dialogs/sysdlg/systableeditor.cpp
@@ -1,4 +1,7 @@
+#include <QFont>
+#include <QVBoxLayout>
 #include "systableeditor.h"
+#include "../../widgets/s_tqlabel.h"
 
 systableeditor::systableeditor(QWidget *parent) :
     QDialog(parent)
@@ -7,5 +10,13 @@ systableeditor::systableeditor(QWidget *parent) :
 
 void systableeditor::SetupUI(QString tblename)
 {
+    QVBoxLayout *lyout = new QVBoxLayout;
+    s_tqLabel *lbl = new s_tqLabel("Редактор таблиц");
+    QFont font;
+    font.setPointSize(15);
+    lbl->setFont(font);
+    lyout->addWidget(lbl);
+    lyout->setAlignment(lbl, Qt::AlignRight);
 
+    setLayout(lyout);
 }

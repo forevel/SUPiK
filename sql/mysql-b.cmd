e01ed1720b018faf06a13e7238fc985554afc948
Anton <forevel@yandex.ru>
2015-10-24 23:49:20 +0300

    #00279_9
    * поправлены командные файлы для sql

--- sql/mysql-b.cmd
+++ sql/mysql-b.cmd
@@ -6,4 +6,4 @@ mysqldump -uanton -p"bIt*rAD()" solidworks > sol.sql
 mysqldump -uanton -p"bIt*rAD()" schemagee > sch.sql
 mysqldump -uanton -p"bIt*rAD()" devices > dev.sql
 mysqldump -uanton -p"bIt*rAD()" tb > tb.sql
-mysqldump -uanton -p"bIt*rAD()" sys > sys.sql
+mysqldump -uanton -p"bIt*rAD()" sysadm > sys.sql

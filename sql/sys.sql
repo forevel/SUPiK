0e49434f455d30f1a36b8690f7cd5874b64d84a3
forevel <vao@asu-vei.ru>
2015-10-25 22:04:15 +0300

    #00280_1
    * backup sql

--- sql/sys.sql
+++ sql/sys.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.6.22, for Win64 (x86_64)
 --
--- Host: localhost    Database: sys
+-- Host: localhost    Database: sysadm
 -- ------------------------------------------------------
 -- Server version	5.6.22
 

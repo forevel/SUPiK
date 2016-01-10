1ea8361c5628331ed531803c8b99692b71e794e3
forevel <vao@asu-vei.ru>
2016-01-10 23:46:38 +0300

    1.0.0294-1
    + сделано новое дерево-таблица treeview+treemodel+treeitem
    + имплементировано пока только в dir_maindialog
    * в s_sql необходимо перевести все коды возврата в осмысленные define
    * разобраться, почему treeview отличается по внешнему виду от s_tqtableview
    * доделать treemodel, чтобы он мог принимать деревья типа 2

---
+++ models/griddelegate.h
@@ -0,0 +1,30 @@
+#ifndef S_DUNIVERSAL_H
+#define S_DUNIVERSAL_H
+
+#include <QStyledItemDelegate>
+#include "../gen/publicclass.h"
+#include "../widgets/s_tqchoosewidget.h"
+
+class s_duniversal : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    explicit s_duniversal(QObject *parent = 0);
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    void setEditorData (QWidget *editor, const QModelIndex &index) const;
+    void setModelData (QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
+    virtual void paint (QPainter *painter, const QStyleOptionViewItem& option, const QModelIndex& index ) const;
+
+public slots:
+
+protected:
+
+private slots:
+    void CommitChanges(QVariant);
+
+private:
+    mutable PublicClass::fieldformat ff;
+    mutable s_tqChooseWidget *wdgt;
+};
+
+#endif // S_DUNIVERSAL_H
